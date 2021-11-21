033817962741fa8218b49ddd0dc3cebe140598ea
alex198000 <85245556+alex198000@users.noreply.github.com>
2021-11-21 20:45:48 +0200

    uiFix

--- New Unity Project/Assets/Scripts/Menu/MenuButtonScript.cs
+++ New Unity Project/Assets/Scripts/Menu/MenuButtonScript.cs
@@ -55,33 +55,82 @@ namespace Menu
         //}
         public void SaveSettings()
         {
-            _uiSettings.Min = Convert.ToInt32(_uiSettings.MinText.text);
-            _settingsControl.Min = _uiSettings.Min;
-            PlayerPrefs.SetInt("Min", _uiSettings.Min);
+            if(_uiSettings.MinText.text.Length == 0)
+            {
+                _uiSettings.MinText.text = _settingsControl.Min.ToString();
+            }
+            else
+            {
+                _uiSettings.Min = Convert.ToInt32(_uiSettings.MinText.text);
+                _settingsControl.Min = _uiSettings.Min;
+                PlayerPrefs.SetInt("Min", _uiSettings.Min);
+            }
 
-            _uiSettings.SpeedEnemy = Convert.ToSingle(_uiSettings.SpeedEnemyText.text);     // float.Parse(_uiSettings.SpeedEnemyText.text);
-            _settingsControl.SpeedEnemy = _uiSettings.SpeedEnemy;
-            PlayerPrefs.SetFloat("SpeedEnemy", _uiSettings.SpeedEnemy);
+            if (_uiSettings.SpeedEnemyText.text.Length == 0)
+            {
+                _uiSettings.SpeedEnemyText.text = _settingsControl.SpeedEnemy.ToString();
+            }
+            else
+            {
+                _uiSettings.SpeedEnemy = Convert.ToSingle(_uiSettings.SpeedEnemyText.text);     // float.Parse(_uiSettings.SpeedEnemyText.text);
+                _settingsControl.SpeedEnemy = _uiSettings.SpeedEnemy;
+                PlayerPrefs.SetFloat("SpeedEnemy", _uiSettings.SpeedEnemy);
+            }
 
-            _uiSettings.GippoSpeed = Convert.ToSingle(_uiSettings.GippoSpeedText.text);
-            _settingsControl.GippoSpeed = _uiSettings.GippoSpeed;
-            PlayerPrefs.SetFloat("GippoSpeed", _uiSettings.GippoSpeed);
+            if (_uiSettings.GippoSpeedText.text.Length == 0)
+            {
+                _uiSettings.GippoSpeedText.text = _settingsControl.GippoSpeed.ToString();
+            }
+            else
+            {
+                _uiSettings.GippoSpeed = Convert.ToSingle(_uiSettings.GippoSpeedText.text);
+                _settingsControl.GippoSpeed = _uiSettings.GippoSpeed;
+                PlayerPrefs.SetFloat("GippoSpeed", _uiSettings.GippoSpeed);
+            }
 
-            _uiSettings.ScoreToWin = Convert.ToInt32(_uiSettings.ScoreToWinText.text);
-            _settingsControl.ScoreToWin = _uiSettings.ScoreToWin;
-            PlayerPrefs.SetInt("ScoreToWin", _uiSettings.ScoreToWin);
+            if (_uiSettings.ScoreToWinText.text.Length == 0)
+            {
+                _uiSettings.ScoreToWinText.text = _settingsControl.ScoreToWin.ToString();
+            }
+            else
+            {
+                _uiSettings.ScoreToWin = Convert.ToInt32(_uiSettings.ScoreToWinText.text);
+                _settingsControl.ScoreToWin = _uiSettings.ScoreToWin;
+                PlayerPrefs.SetInt("ScoreToWin", _uiSettings.ScoreToWin);
+            }
 
-            _uiSettings.ScoreToHitEnemy = Convert.ToInt32(_uiSettings.ScoreToHitEnemyText.text);
-            _settingsControl.ScoreToHitEnemy = _uiSettings.ScoreToHitEnemy;
-            PlayerPrefs.SetInt("ScoreToHitEnemy", _uiSettings.ScoreToHitEnemy);
+            if (_uiSettings.ScoreToHitEnemyText.text.Length == 0)
+            {
+                _uiSettings.ScoreToHitEnemyText.text = _settingsControl.ScoreToHitEnemy.ToString();
+            }
+            else
+            {
+                _uiSettings.ScoreToHitEnemy = Convert.ToInt32(_uiSettings.ScoreToHitEnemyText.text);
+                _settingsControl.ScoreToHitEnemy = _uiSettings.ScoreToHitEnemy;
+                PlayerPrefs.SetInt("ScoreToHitEnemy", _uiSettings.ScoreToHitEnemy);
+            }
 
-            _uiSettings.SpeedRate = Convert.ToSingle(_uiSettings.SpeedRateText.text);
-            _settingsControl.SpeedRate = _uiSettings.SpeedRate;
-            PlayerPrefs.SetFloat("SpeedRate", _uiSettings.SpeedRate);
+            if (_uiSettings.SpeedRateText.text.Length == 0)
+            {
+                _uiSettings.SpeedRateText.text = _settingsControl.SpeedRate.ToString();
+            }
+            else
+            {
+                _uiSettings.SpeedRate = Convert.ToSingle(_uiSettings.SpeedRateText.text);
+                _settingsControl.SpeedRate = _uiSettings.SpeedRate;
+                PlayerPrefs.SetFloat("SpeedRate", _uiSettings.SpeedRate);
+            }
 
-            _uiSettings.SpeedEnemyAttack = Convert.ToSingle(_uiSettings.SpeedEnemyAttackText.text);
-            _settingsControl.SpeedEnemyAttack = _uiSettings.SpeedEnemyAttack;
-            PlayerPrefs.SetFloat("SpeedEnemyAttack", _uiSettings.SpeedEnemyAttack);
+            if (_uiSettings.SpeedEnemyAttackText.text.Length == 0)
+            {
+                _uiSettings.SpeedEnemyAttackText.text = _settingsControl.SpeedEnemyAttack.ToString();
+            }
+            else
+            {
+                _uiSettings.SpeedEnemyAttack = Convert.ToSingle(_uiSettings.SpeedEnemyAttackText.text);
+                _settingsControl.SpeedEnemyAttack = _uiSettings.SpeedEnemyAttack;
+                PlayerPrefs.SetFloat("SpeedEnemyAttack", _uiSettings.SpeedEnemyAttack);
+            }            
         }
 
         public void MusicButton()
